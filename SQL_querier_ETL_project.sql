db6dab6f826a08ebc1051c6e9b64c3b0bd662fb4
jegrossman2004 <157088434+jegrossman2004@users.noreply.github.com>
2024-04-06 14:58:28 -0400

    Update SQL_querier_ETL_project.sql

--- SQL_querier_ETL_project.sql
+++ SQL_querier_ETL_project.sql
@@ -1,14 +1,16 @@
 CREATE DATABASE mets_data;
 USE mets_data;
 
-#QUERY 2: On average, how many scoring plays were there per game when the Mets played any other MLB team?
+#QUERY 1: On average, how many scoring plays were there per game when the Mets played any other MLB team?
 SELECT 
-    AVG(scoring_plays) AS avg_scoring_plays, s.opponent
+    td.team_name, AVG(scoring_plays) AS avg_scoring_plays
 FROM
     mets_data.mets_game_scoring_plays sp
         JOIN
     mets_data.mets_schedule s ON sp.game_number = s.game_number
-GROUP BY s.opponent
+        JOIN
+    team_data td ON s.opponent = td.team_abbreviation
+GROUP BY td.team_name
 ORDER BY avg_scoring_plays;
 
 #QUERY 2: What were the average total runs scored and the average left, center, and right field wall distance of each team's stadium for every MLB team? Ordered by the average distance descending.
@@ -30,9 +32,9 @@ ORDER BY avg_distance DESC;
 
 #QUERY 3: In which games did MLB.com NOT upload 10 highlights from the game??
 SELECT 
-    COUNT(h.highlight_links) AS num_highlights,
     s.game_number,
-    s.date
+    s.date,
+    COUNT(h.highlight_links) AS num_highlights
 FROM
     mets_highlights h
         JOIN
